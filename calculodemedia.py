6d75198f3129814d257383cca80a7b98856c132b
SamuelSantanaesilva <114364164+SamuelSantanaesilva@users.noreply.github.com>
2025-01-08 16:41:00 -0300

    ADICIONEI O CALCULO

---
+++ calculodemedia.py
@@ -0,0 +1,15 @@
+Objeto a ser calculado  = float(input("objeto1: ")
+Objeto a ser calculado= float(input('objeto2: '))
+
+simulado = float(input('objeto simulado: '))
+
+ac1 = float(input('objeto AC1: '))
+
+ac2 = float(input('objeto AC2: '))
+
+
+media1 = (objeto1+objeto2)/2
+mediaAC = (ac1+ac2)/2
+mediaf = (media1+simulado+mediaAC)/3 
+
+Print(Mediaf)
